30b345d792d57a6ca2fb984286c9150e53538798
Yen Lee <yenny0104@gmail.com>
2022-12-19 16:37:05 -0500

    start erb file

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/sessions_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+       username = params[:user][:username]
+       password = params[:user][:password]
+       @user = User.find_by_credentials(username, password)
+
+       if @user
+            redirect_to cats_url
+       else
+            redirect_to new_session_url
+       end
+    end
+
+    def destroy
+    end
+
 end
