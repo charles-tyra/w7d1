c4e9d6fabd6357ffb868d666027121f7c8370512
Charles Tyra <charlestyra@MacBook-Pro.local>
2022-12-19 16:04:52 -0500

    creation of session and user controller

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+   def new
+      @user = User.new
+      render :new
+   end
+
+   def create
+      @user = User.new(user_params)
+
+      if @user.save
+         login!(@user)
+         redirect_to user_url(@user)
+
+      else
+   end
+
+
+   private
+   def user_params
+      params.require(:user).permit(:username, :password)
+   end
+end
